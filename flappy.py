034f96828798441c6e2c956babcd73aead124326
Ifeanyi <ifeanyio569@outlook.com>
2022-10-17 15:52:36 +0100

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -1,5 +1,39 @@
 import pygame, random, time
 from pygame.locals import *
+from pygame import mixer
+  
+# Starting the mixer
+mixer.init()
+  
+# Loading the song
+mixer.music.load("buster_rock.mp3")
+  
+# Setting the volume
+mixer.music.set_volume(0.7)
+  
+# Start playing the song
+mixer.music.play()
+  
+# infinite loop
+while True:
+      
+    print("Press 'p' to pause, 'r' to resume")
+    print("Press 'e' to exit the program")
+    query = input("  ")
+      
+    if query == 'p':
+  
+        # Pausing the music
+        mixer.music.pause()     
+    elif query == 'r':
+  
+        # Resuming the music
+        mixer.music.unpause()
+    elif query == 'e':
+  
+        # Stop the mixer
+        mixer.music.stop()
+        break
 
 #VARIABLES
 SCREEN_WIDHT = 400
